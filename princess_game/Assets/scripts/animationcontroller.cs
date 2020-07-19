d243e75e79e69b4ff4f1c27e78792d3fa4082a62
anchitsh <31262184+anchitsh@users.noreply.github.com>
2020-07-19 20:38:52 +0530

    fixed animation bugs

---
+++ princess_game/Assets/scripts/animationcontroller.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationcontroller : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Animator ani;
+    private SpriteRenderer spriteren;
+
+    float x;
+    bool goinup, comingdown, landed, gp, block, attack, run, walk;
+    private void Awake()
+    {
+        spriteren = GetComponent<SpriteRenderer>();
+
+    }
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        ani = GetComponent<Animator>();
+        ani.SetInteger("jumpstate", 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.GetAxisRaw("Horizontal");
+        goinup = charController.goinup;
+        comingdown = charController.comingdown;
+        landed = charController.landed;
+        gp = charController.groundpound;
+        block = charController.block;
+        attack = charController.attack;
+        run = charController.run;
+        walk = charController.walk;
+
+        if (block == false )
+        {
+            bool flipsorite = (spriteren.flipX ?  (x < -0.01f) :  (x > 0.01f));
+            if (flipsorite == true)
+            {
+                spriteren.flipX = !spriteren.flipX;
+            }
+            /*
+            if (x == 0)
+            {
+                ani.SetBool("idle", true);
+                ani.SetBool("run", false);
+            }
+            else if (x != 0)
+            {
+                ani.SetBool("run", true);
+                ani.SetBool("idle", false);
+            }*/
+
+            if(run == false && walk == false)
+            {
+                ani.SetBool("idle", true);
+                ani.SetBool("run", false);
+                ani.SetBool("walk", false);
+            }
+            else if(run)
+            {
+                ani.SetBool("run", true);
+                ani.SetBool("idle", false);
+                ani.SetBool("walk", false);
+            }
+            else if (walk)
+            {
+                ani.SetBool("walk", true);
+                ani.SetBool("idle", false);
+                ani.SetBool("run", false);
+            }
+
+
+
+        }
+        if(block == true)
+        {
+            ani.SetBool("block", true);
+        }
+        else if (block == false)
+        {
+            ani.SetBool("block", false);
+        }
+
+        if(attack == true)
+        {
+            ani.SetBool("attack", true);
+        }
+        else if (attack == false)
+        {
+            ani.SetBool("attack", false);
+        }
+        if (gp)
+        {
+            goinup = false;
+            comingdown = false;
+            ani.SetInteger("jumpstate", 4);
+        }
+        if (goinup)
+        {
+            ani.SetInteger("jumpstate", 1);
+
+        }
+
+        else if (comingdown)
+        {
+            ani.SetInteger("jumpstate", 2);
+
+        }
+
+        else if (landed)
+        {
+            ani.SetInteger("jumpstate", 3);
+        }
+
+
+    }
+}
